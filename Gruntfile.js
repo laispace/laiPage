2455933f23b28497d32b2d5f5d442d2786fb6b5d
laispace <qingsheng.lai@gmail.com>
2015-01-12 00:47:26 +0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,94 @@
+var autoprefixer = require('autoprefixer-core');
+
+module.exports = function(grunt){
+    grunt.initConfig({
+        // pkg: grunt.file.readJSON('package.json'),
+
+        clean: {
+            download: ['public/download'],
+            jmui: ['public/JMUI']
+        },
+
+
+        sass: {
+            dist: {
+                options: {
+                    style: 'expanded'
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'css',
+                    src: ['**/*.scss'],
+                    dest: 'css',
+                    ext: '.css'
+                }]
+            }
+        },
+
+        postcss: {
+            options: {
+                processors: [
+                    autoprefixer({ browsers: ['last 5 versions'] }).postcss
+                ]
+            },
+            dist: { src: 'css/*.css' }
+        },
+
+        csstidy: {
+            dynamic_mappings: {
+                expand: true,
+                cwd: 'public/css/',
+                src: ['**/*.scss'],
+                dest: 'public/css/',
+                ext: '.scss'
+            }
+        },
+
+        jshint: {
+            files: ['bin/www', 'app.js', 'config.js', 'Gruntfile.js', 'public/js/*.js', 'routes/*.js', 'util/*.js']
+        },
+
+        watch: {
+            html: {
+                files: ['**/*.html'],
+                options: {
+                    livereload: true
+                }
+            },
+            //js: {
+            //    files: ['bin/www', 'app.js', 'config.js', 'Gruntfile.js', 'public/js/*.js', 'routes/*.js', 'util/*.js'],
+            //    tasks: ['jshint'],
+            //    options: {
+            //        livereload: true
+            //    }
+            //},
+
+            //css: {
+            //    files: ['css/*.css'],
+            //    options: {
+            //        livereload: true
+            //    }
+            //},
+            scss: {
+                files: ['css/*.scss'],
+                tasks: ['sass','postcss']
+            }
+        }
+
+
+    });
+
+    //grunt.loadNpmTasks('grunt-contrib-clean');
+    //grunt.loadNpmTasks('grunt-contrib-copy');
+    //grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    //grunt.loadNpmTasks('grunt-at-csstidy');
+    grunt.loadNpmTasks('grunt-postcss');
+    //grunt.loadNpmTasks('grunt-nodemon');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    //grunt.loadNpmTasks('grunt-open');
+
+
+    // 生产环境下执行 grunt
+    grunt.registerTask('default', ['watch']);
+};
---
+++ js/lai-page.js
@@ -0,0 +1,135 @@
+function Page(opts) {
+    this.opts = opts;
+
+    this.init();
+    this.bindEvents();
+}
+
+Page.prototype.init = function () {
+    var opts = this.opts;
+
+    this.width = $(window).width();
+    this.height = $(window).height();
+    this.$container = opts.container ? $(opts.container) : $('.pages');
+    this.$pages = this.$container.find('.page');
+
+    this.currentPageIndex = opts.currentPageIndex ? opts.currentPageIndex : 0;
+    this.$currentPage = this.$pages.eq(this.currentPageIndex);
+
+    this.$container.width(this.width);
+    this.$container.height(this.height);
+
+    this.$container.find('[data-animation]').each(function () {
+        var animations = $(this).data('animation');
+        $(this).addClass(animations);
+    });
+    this.to(0);
+};
+
+Page.prototype.to = function (pageIndex) {
+    this.$currentPage.removeClass('active');
+    var $animatedElements = this.$currentPage.find('[data-animation]');
+    $animatedElements.removeClass('animated');
+
+    this.$currentPage = this.$pages.eq(pageIndex);
+    this.$currentPage.addClass('active');
+    var $animatedElements = this.$currentPage.find('[data-animation]');
+    $animatedElements.addClass('animated');
+};
+
+Page.prototype.next = function () {
+    var nextPageIndex  = this.currentPageIndex + 1;
+    if (nextPageIndex >= this.$pages.length) {
+        nextPageIndex = 0;
+    }
+    this.to(nextPageIndex);
+    this.currentPageIndex  = nextPageIndex;
+};
+
+Page.prototype.prev = function () {
+    var prevPageIndex  = this.currentPageIndex - 1;
+    if (prevPageIndex < 0) {
+        prevPageIndex = this.$pages.length - 1;
+    }
+    this.to(prevPageIndex);
+    this.currentPageIndex  = prevPageIndex;
+};
+
+Page.prototype.bindEvents = function () {
+    var self = this;
+    this.$container.on('touchmove', function (e) {
+        // fix ios
+        if ($.os.ios) {
+            e.preventDefault();
+        }
+    });
+    this.$container.on('swipeUp', function (e) {
+        self.$pages.removeClass('up down left right');
+        self.$pages.addClass('up');
+        self.next();
+    });
+    this.$container.on('swipeDown', function (e) {
+        self.$pages.removeClass('up down left right');
+
+        self.$pages.addClass('down');
+        self.prev();
+    });
+    this.$container.on('swipeLeft', function (e) {
+        self.$pages.removeClass('up down left right');
+
+        self.$pages.addClass('left');
+        self.prev();
+    });
+    this.$container.on('swipeRight', function (e) {
+        self.$pages.removeClass('up down left right');
+        self.$pages.addClass('right');
+        self.next();
+    });
+};
+
+var opts = {}
+var page = new Page(opts);
+
+// dev tools
+var Dev = {};
+Dev.setRandomBgColor = function (selector) {
+    $(selector).each(function () {
+        var rgba = Dev.getRandomBgColor();
+        $(this).css('background-color', rgba);
+    })
+};
+Dev.getRandomBgColor = function () {
+    var r = Dev.getRanDomNumber(0,255);
+    var g = Dev.getRanDomNumber(0,255);
+    var b = Dev.getRanDomNumber(0,255);
+    var a = Dev.getRanDomNumber(50,100) / 100;
+    return 'rgba(' + r + ',' + g + ',' + b + ',' + a +')';
+};
+
+Dev.getRanDomNumber = function (min, max) {
+    var range = max - min;
+    var result = Math.floor(Math.random() * range) + min;
+    return result;
+};
+
+Dev.setRandomBgColor('.page');
+
+var Log = {};
+Log.show = function (data) {
+    var div = document.getElementById('log');
+    if (!div) {
+        div = document.createElement('div');
+        div.id = 'log';
+        div.style.position = 'fixed';
+        div.style.zIndex = 9999;
+        div.style.top = 0;
+        div.style.right = 0;
+        document.body.appendChild(div);
+    }
+    div.style.backgroundColor = Dev.getRandomBgColor();
+    div.innerHTML = data;
+};
+
+
+
+
