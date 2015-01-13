42c56abc95030b2c028fff8fb706d46345c2a793
laispace <qingsheng.lai@gmail.com>
2015-01-14 01:28:31 +0800

    add demo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,9 @@
 var autoprefixer = require('autoprefixer-core');
 
 module.exports = function(grunt){
+    require('load-grunt-tasks')(grunt);
+
     grunt.initConfig({
-        // pkg: grunt.file.readJSON('package.json'),
-
-        clean: {
-            download: ['public/download'],
-            jmui: ['public/JMUI']
-        },
-
-
         sass: {
             dist: {
                 options: {
@@ -45,7 +39,7 @@ module.exports = function(grunt){
         },
 
         jshint: {
-            files: ['bin/www', 'app.js', 'config.js', 'Gruntfile.js', 'public/js/*.js', 'routes/*.js', 'util/*.js']
+            files: ['Gruntfile.js', 'js/*.js']
         },
 
         watch: {
@@ -55,40 +49,17 @@ module.exports = function(grunt){
                     livereload: true
                 }
             },
-            //js: {
-            //    files: ['bin/www', 'app.js', 'config.js', 'Gruntfile.js', 'public/js/*.js', 'routes/*.js', 'util/*.js'],
-            //    tasks: ['jshint'],
-            //    options: {
-            //        livereload: true
-            //    }
-            //},
-
-            //css: {
-            //    files: ['css/*.css'],
-            //    options: {
-            //        livereload: true
-            //    }
-            //},
             scss: {
                 files: ['css/*.scss'],
-                tasks: ['sass','postcss']
+                tasks: ['newer:sass','newer:postcss'],
+                options: {
+                    livereload: true
+                }
             }
         }
 
 
     });
 
-    //grunt.loadNpmTasks('grunt-contrib-clean');
-    //grunt.loadNpmTasks('grunt-contrib-copy');
-    //grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    //grunt.loadNpmTasks('grunt-at-csstidy');
-    grunt.loadNpmTasks('grunt-postcss');
-    //grunt.loadNpmTasks('grunt-nodemon');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    //grunt.loadNpmTasks('grunt-open');
-
-
-    // 生产环境下执行 grunt
     grunt.registerTask('default', ['watch']);
 };
